06f7d5ec6d2e25a0a19bf743590838487ef6e895
keagud <keagud@alloquor.net>
2024-04-08 20:19:17 -0500

    add JS to remove CSS on April 9

--- static/script.js
+++ static/script.js
@@ -1,5 +1,18 @@
-(async () => {
+const setQuote = () => (async () => {
   const quoteElement = document.querySelector(".dynamic-quote");
+
+  if (isNakedCSS()) {
+
+    let nakedLink = document.createElement('a');
+    nakedLink.innerText = "happy naked CSS day!"
+    nakedLink.href = "https://css-naked-day.github.io/";
+
+    quoteElement.appendChild(nakedLink);
+    return;
+
+  }
+
+
   await fetch("/quotes.json")
     .then(r => r.json())
     .then(j => j["quotes"])
@@ -9,3 +22,39 @@
     })
 })();
 
+
+
+function removeCSS() {
+  let styleLinks = document.querySelectorAll('link[rel="stylesheet"]');
+
+  let styleBlocks = document.querySelectorAll('style');
+
+  for (const e of [...styleLinks, ...styleBlocks]) {
+    e.parentNode.removeChild(e);
+  }
+}
+
+
+function isNakedCSS() {
+  let today = new Date();
+
+
+  console.log(today.getUTCMonth());
+  console.log(today.getUTCDate());
+  //getUTCMonth is zero-indexed, so 3 is April
+  return (today.getUTCMonth() === 3 && Math.abs(today.getUTCDate() - 9 ) <= 1);
+
+
+
+}
+
+
+function main() {
+  setQuote();
+  if (isNakedCSS()) { 
+    console.log("It's april 9");
+
+    removeCSS(); }
+}
+
+main();
