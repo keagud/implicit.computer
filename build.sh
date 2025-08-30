11e35d4825f1864b1a0d63a2e5448267fc5c3544
keagud <keagud@alloquor.net>
2025-08-30 11:54:38 -0700

    update build script to prettify markup

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 ZOLA_VERSION=$(cat zola_version | xargs )
 curl -L "https://github.com/getzola/zola/releases/download/v$ZOLA_VERSION/zola-v$ZOLA_VERSION-x86_64-unknown-linux-gnu.tar.gz" | tar xz && ./zola build
+
+npx prettier -w public/**/*.{html,css,js} && echo "Prettified"
