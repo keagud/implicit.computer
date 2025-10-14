0986a797a064b2df9c0bbbf57d14eea915d957a9
keagud <keagud@alloquor.net>
2025-10-14 13:08:18 -0700

    update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+
+# get the latest blogroll from the file host
+curl -L "https://files.implicit.computer/f/feeds.opml.xml" > /tmp/feeds && mv /tmp/feeds ./static/feeds.opml.xml
+
+# run the site build
 ZOLA_VERSION=$(cat zola_version | xargs )
 curl -L "https://github.com/getzola/zola/releases/download/v$ZOLA_VERSION/zola-v$ZOLA_VERSION-x86_64-unknown-linux-gnu.tar.gz" | tar xz && ./zola build
 
+# post-build script
 chmod +x ./scripts/post_build.py && ./scripts/post_build.py
 
 npx prettier -w public/**/*.{html,css,js} && echo "Prettified"
